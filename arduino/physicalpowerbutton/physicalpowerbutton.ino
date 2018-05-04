9bc33f4f20620ea5cf8743af9ef16d42e39d9d90
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-05-04 19:02:58 +0200

    Adding shutdown function

--- arduino/physicalpowerbutton/physicalpowerbutton.ino
+++ arduino/physicalpowerbutton/physicalpowerbutton.ino
@@ -2,13 +2,16 @@
 
 //String str;
 //String strlast;
-int started;
+int started = 0; 
+int firstBoot = 1; //on first boot the RPI start automaticaly on power plug
 
 int pushButton = 2;
 int led_r = 3;
 int led_g = 4;
-int rpiPowerControl = 5;
-int previousState = LOW;
+int rpiPowerUp = 5;
+int rpiPowerDown = 6;
+int rpiStat = 7;
+//int previousState = LOW;
 //boolean stringComplete = false;
 
 void wakeUp()
@@ -22,78 +25,49 @@ void setup() {
   pinMode(pushButton, INPUT_PULLUP);
   pinMode(led_r, OUTPUT);
   pinMode(led_g, OUTPUT);  
-  pinMode(rpiPowerControl, OUTPUT);
+  pinMode(rpiPowerUp, OUTPUT);
+  pinMode(rpiPowerDown, OUTPUT);  
+  pinMode(rpiStat, INPUT);
   digitalWrite(led_r, LOW);
   digitalWrite(led_g, LOW);
-  digitalWrite(rpiPowerControl, HIGH);
-  /*started = 0;
-  str.reserve(200);
-  strlast.reserve(200);*/
+  digitalWrite(rpiPowerUp, HIGH);
+  digitalWrite(rpiPowerUp, HIGH);
+
+}
+
+void dotPrint()
+{
+   while (digitalRead(rpiStat) != LOW)
+   {
+    Serial.print(".");
+    delay(1000);
+   }
+   Serial.println("");
 }
 
 void loop()
 {
-    attachInterrupt(0, wakeUp, LOW);
-    int actionDone=0;
-
- /*  if (Serial.available() > 0)
-  {
-    strlast = str;
-    str = Serial.readStringUntil('\n');
-
-    if (started)
-    {
-      if (str.endsWith(" reboot: Power down\r"))
-      {
-        digitalWrite(led, LOW);
-        digitalWrite(rpiPowerControl, HIGH);
-        started = 0;
-      }
-    }
-    else
-    {
-      if (str == "raspberrypi login: ")
-      {
-        digitalWrite(led, HIGH);
-        started = 1;
-      }
-    }
-  }*/
-
+  attachInterrupt(0, wakeUp, LOW);
+  int actionDone=0;
+  
   int buttonState = digitalRead(pushButton);
-  /*if (buttonState == 0)
+  if ( buttonState == 0 || firstBoot == 1)
   {
-    if (started == 1)
-    {
-      //      Serial.println("1[" + strlast + "]");
-      //      Serial.println("2[" + str + "]");
-      //      delay(1000);
-      Serial.print("pi\n");
-      delay(1000);
-      Serial.print("raspberry\n");
-      delay(1000);
-      Serial.print("sudo shutdown -h now\n");
-      //when started
-      //<raspberrypi login: >
-      //when stopped
-      //<[  390.039293] reboot: Power down>
-    }
-    else
-    {
-      digitalWrite(rpiPowerControl, LOW);
-      delay(1000);
-    }
-  }*/
-  if ( buttonState == 0 )
-  {
-    Serial.println("Button Pressed");
+    if ( firstBoot == 1 ) { Serial.println("Button Pressed"); } else { Serial.println("Plugged"); }
     if ( started == 0 && actionDone == 0 )
     {
        Serial.println("Starting the pi");
-       // passing to low state for .5s then return to high state
-       digitalWrite(rpiPowerControl, LOW);
-       delay(500);
-       digitalWrite(rpiPowerControl, HIGH);
+       if ( firstBoot != 1 )
+       {
+         // passing to low state for .2s then return to high state
+         digitalWrite(rpiPowerUp, LOW);
+         delay(200);
+         digitalWrite(rpiPowerUp, HIGH);
+       }
+       // wait for RPI complete startup
+       Serial.print("Waiting for RPI complete startup ");
+       dotPrint;
+       Serial.println("Startup complete");
        started=1;
        actionDone=1;
     }
@@ -101,16 +75,23 @@ void loop()
      {
        // not implemented yet
        Serial.println("Shutdown the pi");
+       digitalWrite(rpiPowerDown, LOW);
+       // wait for RPI complete shutdown
+       Serial.print("Waiting for RPI complete startup ");
+       dotPrint;
+       Serial.println("Shutdown complete");
        started=0;
        actionDone=1;
      }
   }
-  //wait until the button is released
+  /* //wait until the button is released
   while (digitalRead(pushButton)==0)
   {
     //do nothing
   }
-  delay(1000);  
+  delay(1000);*/
+  firstBoot=0;
+  // go to sleep mode  
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   detachInterrupt(0);  
 }
